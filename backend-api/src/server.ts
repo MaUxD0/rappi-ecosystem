124e7ca699d2cabd5beb0752a773c90ae32fab62
Mauricio Barrera <kevinmauriciob3@gmail.com>
2026-03-15 21:30:43 -0500

    add endpoints

--- backend-api/src/server.ts
+++ backend-api/src/server.ts
@@ -1,8 +1,16 @@
 import app from "./app";
 import dotenv from "dotenv";
+import authRoutes from "./features/auth/auth.routes";
 import storeRoutes from "./routes/store.routes";
+import productRoutes from "./routes/product.routes";
+import orderRoutes from "./routes/order.routes";
+import deliveryRoutes from "./routes/delivery.routes";
 dotenv.config();
+app.use("/auth", authRoutes);
 app.use("/stores", storeRoutes);
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+app.use("/deliveries", deliveryRoutes);
 const PORT = 3000;
 
 app.listen(PORT, () => {
